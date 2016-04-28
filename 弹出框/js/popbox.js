28634da0cafe449a851780ab13056b1130c958e5
m.max <1036310327@qq.com>
2016-04-28 14:08:30 +0800

    修改弹窗
    Signed-off-by: m.max <1036310327@qq.com>

--- 弹出框/js/popbox.js
+++ 弹出框/js/popbox.js
@@ -1,28 +1,32 @@
 var popbox = {
-  init: function () {
+  init: function(maskhide) {
     var $this = this;
     var mask = '<div class="mask"></div>';
     var l = $('.mask').length;
-    if(l){
+    if (l) {
       $('.mask').addClass('active');
     } else {
       $('body').append(mask);
     }
-    $('.mask-box').click(function (e) {
-      var l = $(e.target).find('.msg').length;
-      if(l || $(e.target).hasClass('msg') || $(e.target).hasClass('hide-pop')){
-        $this.hide();
-      }
-    });
+    if (maskhide) {
+      $('.mask-box').click(function(e) {
+        var l = $(e.target).find('.msg').length;
+        if (l || $(e.target).hasClass('msg') || $(e.target).hasClass('hide-pop')) {
+          $this.hide();
+        }
+      });
+    }
+
   },
-  show: function (id) {
+  show: function(id) {
     $('body').addClass('overflow');
     $(id).addClass('active');
     $('.mask').addClass('active');
   },
-  hide: function () {
+  hide: function(id) {
+    var o = id || '.mask-box';
     $('body').removeClass('overflow');
-    $('.mask-box').removeClass('active');
+    $(o).removeClass('active');
     $('.mask').removeClass('active');
   }
 };
