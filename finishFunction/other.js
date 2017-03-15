051b0ddcdd277866ca30a8e25c49c0c13e1ec074
m.max <1036310327@qq.com>
2017-03-15 18:09:58 +0800

    video进入全屏，退出全屏 为空判断

--- finishFunction/other.js
+++ finishFunction/other.js
@@ -1132,3 +1132,102 @@ function opDate(date, days) {
   var m = d.getMonth() + 1;
   return d.getFullYear() + '/' + m + '/' + d.getDate();
 }
+
+// 模拟setInterval保证每次调用之间的间隔相同
+function interval(func, wait) {
+  var interv = function() {
+    func.call(null);
+    setTimeout(interv, wait);
+  };
+  setTimeout(interv, wait);
+}
+
+//反射調用
+var invokeFieldOrMethod = function(element, method) {
+  var usablePrefixMethod;
+  ["webkit", "moz", "ms", "o", ""].forEach(function(prefix) {
+    if (usablePrefixMethod) return;
+    if (prefix === "") {
+      // 无前缀，方法首字母小写
+      method = method.slice(0, 1).toLowerCase() + method.slice(1);
+    }
+    var typePrefixMethod = typeof element[prefix + method];
+    if (typePrefixMethod + "" !== "undefined") {
+      if (typePrefixMethod === "function") {
+        usablePrefixMethod = element[prefix + method]();
+      } else {
+        usablePrefixMethod = element[prefix + method];
+      }
+    }
+  });
+
+  return usablePrefixMethod;
+};
+
+// video全屏，退出全屏
+//進入全屏
+function launchFullscreen(element) {
+  //此方法不可以在異步任務中執行，否則火狐無法全屏
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.oRequestFullscreen) {
+    element.oRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullScreen();
+  } else {
+
+    var docHtml = document.documentElement;
+    var docBody = document.body;
+    var videobox = document.getElementById('videobox');
+    var cssText = 'width:100%;height:100%;overflow:hidden;';
+    docHtml.style.cssText = cssText;
+    docBody.style.cssText = cssText;
+    videobox.style.cssText = cssText + ';' + 'margin:0px;padding:0px;';
+    document.IsFullScreen = true;
+
+  }
+}
+//退出全屏
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.oRequestFullscreen) {
+    document.oCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else {
+    var docHtml = document.documentElement;
+    var docBody = document.body;
+    var videobox = document.getElementById('videobox');
+    docHtml.style.cssText = "";
+    docBody.style.cssText = "";
+    videobox.style.cssText = "";
+    document.IsFullScreen = false;
+  }
+}
+document.getElementById('fullScreenBtn').addEventListener('click', function() {
+  launchFullscreen(document.getElementById('video'));
+  window.setTimeout(function exit() {
+    //檢查瀏覽器是否處於全屏
+    if (invokeFieldOrMethod(document, 'FullScreen') || invokeFieldOrMethod(document, 'IsFullScreen') || document.IsFullScreen) {
+      exitFullscreen();
+    }
+  }, 5 * 1000);
+}, false);
+
+// 为空判断
+function isBlank(s) {
+  if (s.replace(/^\s+|\s+$/g, '').length === 0 || s === null || s === undefined) {
+    return true;
+  } else {
+    return false;
+  }
+}
