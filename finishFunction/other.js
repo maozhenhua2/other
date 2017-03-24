234c8f07f8addd215866d2ea55789808dcff657b
m.max <1036310327@qq.com>
2017-03-24 16:27:35 +0800

    other.js添加判断移动端

--- finishFunction/other.js
+++ finishFunction/other.js
@@ -1227,3 +1227,24 @@ document.getElementById('fullScreenBtn').addEventListener('click', function() {
   }, 5 * 1000);
 }, false);
 
+// 判断是否是移动端
+function isMobile() {
+  var isMobile = {
+    Android: function() {
+      return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function() {
+      return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function() {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function() {
+      return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function() {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+  };
+  return isMobile;
+}
