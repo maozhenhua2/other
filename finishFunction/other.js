76fa42fe5facfc3ebec3020c4a627cf42d14d337
m.max <1036310327@qq.com>
2017-04-07 19:12:58 +0800

    更新

--- finishFunction/other.js
+++ finishFunction/other.js
@@ -1246,3 +1246,21 @@ function isMobile() {
   };
   return isMobile;
 }
+
+// 取到顶层对象
+// 方法一
+(typeof window !== 'undefined'
+   ? window
+   : (typeof process === 'object' &&
+      typeof require === 'function' &&
+      typeof global === 'object')
+     ? global
+     : this);
+
+// 方法二
+var getGlobal = function () {
+  if (typeof self !== 'undefined') { return self; }
+  if (typeof window !== 'undefined') { return window; }
+  if (typeof global !== 'undefined') { return global; }
+  throw new Error('unable to locate global object');
+};
