6351ac18dfee6fa8a41537c74642e69516ddaa48
maozhenhua2 <1036310327@qq.com>
2017-07-08 10:13:38 +0800

    添ie8 console.log兼容函数
    添加检测手机横屏竖屏检测
    
    Signed-off-by: maozhenhua2 <1036310327@qq.com>

--- finishFunction/other.js
+++ finishFunction/other.js
@@ -1152,8 +1152,6 @@ function onlyInput(select, value, term) {
   });
 }
 
-
-
 // 模拟setInterval保证每次调用之间的间隔相同
 function interval(func, wait) {
   var interv = function() {
@@ -1326,3 +1324,35 @@ function compare(a, b) {
   }
   return v
 }
+//判断手机横竖屏状态：
+function hengshuping() {
+  if (window.orientation == 180 || window.orientation == 0) {
+    alert("竖屏状态！")
+  }
+  if (window.orientation == 90 || window.orientation == -90) {
+    alert("横屏状态！")
+  }
+}
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+
+function orient() {
+  if (window.orientation == 90 || window.orientation == -90) {
+    //ipad、iphone竖屏；Andriod横屏
+    $("body").attr("class", "landscape");
+    orientation = 'landscape';
+    return false;
+  } else if (window.orientation == 0 || window.orientation == 180) {
+    //ipad、iphone横屏；Andriod竖屏
+    $("body").attr("class", "portrait");
+    orientation = 'portrait';
+    return false;
+  }
+}
+//页面加载时调用
+$(function() {
+  orient();
+});
+//用户变化屏幕方向时调用
+$(window).bind('orientationchange', function(e) {
+  orient();
+});
