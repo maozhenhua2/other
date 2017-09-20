dd42b3f7d63b4effd7ddac8a3e8b4a02c00e255e
maozhenhua2 <1036310327@qq.com>
2017-09-20 10:24:00 +0800

    添加判断移动端js

--- finishFunction/other.js
+++ finishFunction/other.js
@@ -1356,3 +1356,25 @@ $(function() {
 $(window).bind('orientationchange', function(e) {
   orient();
 });
+
+// 判断是否是移动端
+function isMobile() {
+  var isMobile = {
+    Android: function () {
+      return navigator.userAgent.match(/Android/i) ? true : false;
+    },
+    BlackBerry: function () {
+      return navigator.userAgent.match(/BlackBerry/i) ? true : false;
+    },
+    iOS: function () {
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
+    },
+    Windows: function () {
+      return navigator.userAgent.match(/IEMobile/i) ? true : false;
+    },
+    any: function () {
+      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
+    }
+  };
+  return isMobile;
+}
