54acce792ee28e2c09650963f2ebd8113685f071
maozhenhua2 <1036310327@qq.com>
2017-12-03 11:03:29 +0800

    function.js 添加 time.js 添加 时间方面的函数 dateFormat/js/d1.js 删除 移动到finishFunction目录中改名为time.js array.js 添加新的方法 other.js 添加新的方法

--- finishFunction/time.js
+++ finishFunction/time.js
@@ -51,3 +51,47 @@ Date.prototype.pattern = function (fmt) {
  var date = new Date();
  console.log(date.pattern('yyyy-MM-dd HH:mm:ss')); // 输出 2016-06-02 10:14:09
  */
+
+/**
+ * @desc   格式化${startTime}距现在的已过时间
+ * @param  {Date} startTime 
+ * <a href='http://www.jobbole.com/members/wx1409399284'>@return</a> {String}
+ */
+function formatPassTime(startTime) {
+    var currentTime = Date.parse(new Date()),
+        time = currentTime - startTime,
+        day = parseInt(time / (1000 * 60 * 60 * 24)),
+        hour = parseInt(time / (1000 * 60 * 60)),
+        min = parseInt(time / (1000 * 60)),
+        month = parseInt(day / 30),
+        year = parseInt(month / 12);
+    if (year) return year + "年前"
+    if (month) return month + "个月前"
+    if (day) return day + "天前"
+    if (hour) return hour + "小时前"
+    if (min) return min + "分钟前"
+    else return '刚刚'
+}
+
+/**
+ * 
+ * @desc   格式化现在距${endTime}的剩余时间
+ * @param  {Date} endTime  
+ * <a href='http://www.jobbole.com/members/wx1409399284'>@return</a> {String}
+ */
+function formatRemainTime(endTime) {
+    var startDate = new Date(); //开始时间
+    var endDate = new Date(endTime); //结束时间
+    var t = endDate.getTime() - startDate.getTime(); //时间差
+    var d = 0,
+        h = 0,
+        m = 0,
+        s = 0;
+    if (t >= 0) {
+        d = Math.floor(t / 1000 / 3600 / 24);
+        h = Math.floor(t / 1000 / 60 / 60 % 24);
+        m = Math.floor(t / 1000 / 60 % 60);
+        s = Math.floor(t / 1000 % 60);
+    }
+    return d + "天 " + h + "小时 " + m + "分钟 " + s + "秒";
+}
