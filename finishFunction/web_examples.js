64651d71d208c13bfd0a1c2ab705636683ef8143
maozhenhua2 <1036310327@qq.com>
2018-04-03 11:50:40 +0800

    update

--- finishFunction/web_examples.js
+++ finishFunction/web_examples.js
@@ -285,33 +285,47 @@ var currying = function(fn) {
 
 //  函数节流
 var throttle = function(fn, interval) {
-
   var __self = fn, // 保存需要被延迟执行的函数引用 
     timer, // 定时器 
     firstTime = true; // 是否是第一次调用 
-
   return function() {
     var args = arguments,
       __me = this;
-
     if (firstTime) { // 如果是第一次调用，不需延迟执行 
       __self.apply(__me, args);
       return firstTime = false;
     }
-
     if (timer) { // 如果定时器还在，说明前一次延迟执行还没有完成 
       return false;
     }
-
     timer = setTimeout(function() { // 延迟一段时间执行 
       clearTimeout(timer);
       timer = null;
       __self.apply(__me, args);
-
     }, interval || 500);
-
   };
+};
 
+var throttleV2 = function(fn, delay, mustRunDelay) {
+  var timer = null;
+  var t_start;
+  return function() {
+    var context = this,
+      args = arguments,
+      t_curr = +new Date();
+    clearTimeout(timer);
+    if (!t_start) {
+      t_start = t_curr;
+    }
+    if (t_curr - t_start >= mustRunDelay) {
+      fn.apply(context, args);
+      t_start = t_curr;
+    } else {
+      timer = setTimeout(function() {
+        fn.apply(context, args);
+      }, delay);
+    }
+  };
 };
 
 // 命名空间
