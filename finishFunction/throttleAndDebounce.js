82fdecbcafd6878a04fc6f8afcf8fd9cb1d38747
maozhenhua2 <1036310327@qq.com>
2018-09-05 10:54:25 +0800

    节流和防抖

---
+++ finishFunction/throttleAndDebounce.js
@@ -0,0 +1,38 @@
+// 定时执行
+function throttle(fn, interval) {
+  var last;
+  var timer;
+  var interval = interval || 200;
+  return function() {
+    var th = this;
+    var args = arguments;
+    var now = +new Date();
+    if (last && now - last < interval) {
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        last = now;
+        fn.apply(th, args);
+      }, interval);
+    } else {
+      last = now;
+      fn.apply(th, args);
+    }
+  }
+}
+
+// 执行最后一次
+function debounce(fn, delay) {
+  var delay = delay || 200;
+  var timer;
+  return function() {
+    var th = this;
+    var args = arguments;
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+      timer = null;
+      fn.apply(th, args);
+    }, delay);
+  };
+}
